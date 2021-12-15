8a6af88f5985b9f51227e5ac32e9dd8960cb1c64
TomMia <TomMiaProCo@gmail.com>
2021-12-15 17:23:36 +0700

    add create new sketchpad

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let wrapperDiv = document.querySelector("#wrapper");
-let numPaintedSquared = 0;
-for(let i = 0; i < 16*16 ; i++){
-    let squareDiv = document.createElement("div");
-    squareDiv.classList.add("square");
-    squareDiv.addEventListener("mouseover", () => {
-        if(squareDiv.style.backgroundColor != "rgb(255, 136, 0)"){
-            squareDiv.style.backgroundColor = "#ff8800";
-            numPaintedSquared++;
-        }
-        if(numPaintedSquared == 16*16){
-            window.location.reload();
-        }
-    })
-
-    wrapperDiv.appendChild(squareDiv);
+let askWidth = () => {
+    let size = prompt("Size of grid");
+    if(size > 100) {
+        alert("Size is exceed the maximum of 100 please try again");
+        askWidth();
+    }
+    return size;
 }
 
+let numPaintedSquared = 0;
+let numSquareOnOneSide = 5;
+function BuildSketchPad(){
+    for(let i = 0; i < numSquareOnOneSide*numSquareOnOneSide ; i++){
+        let squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        let width = Math.floor((100/numSquareOnOneSide)).toString() + "%";
+        squareDiv.setAttribute("style", "width: "+ width);
+
+        squareDiv.addEventListener("mouseover", () => {
+            if(squareDiv.style.backgroundColor != "rgb(255, 136, 0)"){
+                squareDiv.style.backgroundColor = "#ff8800";
+                numPaintedSquared++;
+            }
+            if(numPaintedSquared == numSquareOnOneSide*numSquareOnOneSide){
+                numSquareOnOneSide = askWidth();
+                
+                document
+                    .querySelectorAll(".square")
+                    .forEach(x => x.remove());
+                
+                BuildSketchPad();
+            }
+        })
+
+        wrapperDiv.appendChild(squareDiv);
+    }
+}
+
+BuildSketchPad();
